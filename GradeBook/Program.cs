7a219ca279bed9aacba207408f6bcc1340e1c066
vasilisdmr <vasilisdmr@gmail.com>
2019-12-11 20:49:15 +0200

    Added a list of doubles and found the average.

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+
+            grades.Add(5.1);
+
+            var result = 0.0;
+
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
