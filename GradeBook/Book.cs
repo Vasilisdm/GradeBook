e0d4b19ce07ade0ff0e6d5a4ccfa5dd0370ee6cd
vasilisdmr <vasilisdmr@gmail.com>
2019-12-11 21:38:42 +0200

    Created constructor for the Book class and initialized the grades list, in order to avoid null reference error.

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -5,6 +5,11 @@ namespace GradeBook
 {
     public class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
