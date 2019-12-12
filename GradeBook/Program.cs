2caaea57609650930d011b285c94adac3a07c3f1
vasilisdmr <vasilisdmr@gmail.com>
2019-12-12 23:12:04 +0200

    Added a try catch in order to catch the exceptions

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -19,8 +19,25 @@ namespace GradeBook
                     break;
                 }
 
-                var grade = double.Parse(userInput);
-                book.AddGrade(grade);
+                try
+                {
+                    var grade = double.Parse(userInput);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("****");
+                }
+
+                
             }
 
             var stats = book.GetStatistics();
