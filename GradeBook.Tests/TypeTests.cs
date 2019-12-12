b3b7b4a91b24c56942521dec675e5cf1a0e704ac
vasilisdmr <vasilisdmr@gmail.com>
2019-12-12 21:16:38 +0200

    Wrote a simple test to check the name of two books.

--- GradeBook.Tests/TypeTests.cs
+++ GradeBook.Tests/TypeTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using Xunit;
+
 namespace GradeBook.Tests
 {
     public class TypeTests
     {
-        public TypeTests()
+        [Fact]
+        public void test1()
         {
+            var book = GetBook("Book1");
+            var book2 = GetBook("Book2");
+
+            Assert.Equal("Book1", book.Name);
+            Assert.Equal("Book2", book2.Name);
+
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
         }
     }
 }
