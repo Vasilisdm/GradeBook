9b2a8198e71042f80f66ef6ed30f8666b615924c
vasilisdmr <vasilisdmr@gmail.com>
2019-12-15 20:57:11 +0200

    Used the inmemory book to create an instance.

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -7,7 +7,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Grade Book");
+            var book = new InMemoryBook("Grade Book");
             book.GradeAdded += OnGradedAdded;
 
             EnterGrades(book);
