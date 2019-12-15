cbc58c99fc725183b54ce7737f1c3e11c54e6784
vasilisdmr <vasilisdmr@gmail.com>
2019-12-15 20:59:20 +0200

    Created the abstract class book which is going to be the blueprint about the book items that are going to be created.

---
+++ GradeBook/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GradeBook
+{
+    public abstract class Book : NamedObject
+    {
+        public Book(string name) : base(name)
+        {
+            Name = name;
+        }
+
+        public abstract void AddGrade(double grade);
+    }
+}
