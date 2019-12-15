94c82775b9b2253305e805896d5754a6fba16cc6
vasilisdmr <vasilisdmr@gmail.com>
2019-12-16 00:23:49 +0200

    Implemented the DiskBook class which implements the book abstract class

---
+++ GradeBook/DiskBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GradeBook
+{
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) : base(name)
+        {
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
