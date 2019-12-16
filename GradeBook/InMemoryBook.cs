dd5f98d8a3df2288d326e0aaf29ba97bf1e521f7
vasilisdmr <vasilisdmr@gmail.com>
2019-12-16 21:30:04 +0200

    Removed the uncecessary code that moved into the Statistics class.

--- GradeBook/InMemoryBook.cs
+++ GradeBook/InMemoryBook.cs
@@ -54,37 +54,11 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             Statistics result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
             foreach (var grade in grades)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
-            }
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d > 90:
-                    result.Letter = 'A';
-                    break;
-                case var d when d > 80:
-                    result.Letter = 'B';
-                    break;
-                case var d when d > 70:
-                    result.Letter = 'C';
-                    break;
-                case var d when d > 60:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
+                result.Add(grade);
+            } 
 
             return result;
         }
