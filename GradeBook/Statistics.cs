6dcd5444e0236b33f6a78090106933811df2c6a1
vasilisdmr <vasilisdmr@gmail.com>
2019-12-16 21:29:42 +0200

    Moved code from InMemoryBook to the statistics class.

--- GradeBook/Statistics.cs
+++ GradeBook/Statistics.cs
@@ -5,7 +5,52 @@ namespace GradeBook
     {
         public double High;
         public double Low;
-        public double Average;
-        public char Letter;
+
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d > 90:
+                        return 'A';
+                    case var d when d > 80:
+                        return 'B';
+                    case var d when d > 70:
+                        return 'C'; 
+                    case var d when d > 60:
+                        return 'D'; 
+                    default:
+                        return 'F'; 
+                }
+            }
+
+        }
+        public int Count;
+        public double Sum;
+
+        public void Add(double grade)
+        {
+            Sum += grade;
+            Count += 1;
+            Low = Math.Min(grade, Low);
+            High = Math.Max(grade, High);
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Sum = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
     }
 }
